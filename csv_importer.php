9c47ea23f102f487118fe2e5be86be557b8f21b1
shin1kt <nomeshi74@gmail.com>
2015-09-29 11:15:55 +0900

    LICENSE change

--- csv_importer.php
+++ csv_importer.php
@@ -1,12 +1,36 @@
 <?php
 /*
 Plugin Name: CSV Importer
-Description: Import data as posts from a CSV file. <em>You can reach the author at <a href="mailto:d.v.kobozev@gmail.com">d.v.kobozev@gmail.com</a></em>.
-Version: 0.3.8
-Author: Denis Kobozev
+Description: wordpress plugin csv-importer(https://wordpress.org/plugins/csv-importer/) add a comment meta import.
+Version: 0.1
+Author: shin1kt
 */
 
 /**
+ * Copyright (c) 2015 shin1kt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * <Original code has been created by Denis Kobozev. The original line sense is as follows.>
+ *
+ *
  * LICENSE: The MIT License {{{
  *
  * Copyright (c) <2009> <Denis Kobozev>
